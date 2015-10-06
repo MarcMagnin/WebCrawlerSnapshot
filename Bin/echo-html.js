fe0123059367a1d5629f52e45a59cea3fc34f470
Marc <marc.magnin@gmail.com>
2015-10-06 13:58:57 +0200

    added files

---
+++ Bin/echo-html.js
@@ -0,0 +1,57 @@
+﻿var webPage = require('webpage');
+var page = webPage.create();
+page.settings.resourceTimeout = 50000;
+phantom.outputEncoding = "System";
+var system = require('system');
+var args = system.args;
+
+//if (args.length === 1) {
+//    console.log('Try to pass some arguments when invoking this script!');
+//} else {
+//    args.forEach(function (arg, i) {
+//        console.log(i + ': ' + arg);
+//    });
+//}
+
+
+page.onCallback = function () {
+    console.log(page.content);
+    phantom.exit();
+};
+page.onError = function (msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+
+    if (trace && trace.length) {
+        msgStack.push('TRACE:');
+        trace.forEach(function (t) {
+            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+        });
+    }
+
+    console.error(msgStack.join('\n'));
+    phantom.exit();
+};
+page.onResourceTimeout = function (request) {
+    console.log('Response (#' + request.id + '): ' + JSON.stringify(request));
+    phantom.exit();
+};
+page.open(args[1], function (status) {
+    //console.log('Status: ' + status);
+});
+
+
+
+//function helloWorld() {
+//    console.log(phantom.outputEncoding + ": Benoît éà");
+//}
+//console.log(phantom.outputEncoding + ": æøå");
+
+//helloWorld();
+
+//console.log("\nUsing other encodings...");
+
+//var encodings = ["euc-jp", "sjis", "utf8", "System"];
+//for (var i = 0; i < encodings.length; i++) {
+//    phantom.outputEncoding = encodings[i];
+//    helloWorld();
+//}
---
+++ Handler.ashx
@@ -0,0 +1,42 @@
+﻿<%@ WebHandler Language="C#" Class="Handler" %>
+
+using NReco.PhantomJS;
+using System.Web;
+using System.Text;
+
+public class Handler : IHttpHandler
+{
+
+    public void ProcessRequest(HttpContext context)
+    {
+        var t = new PhantomJS();
+
+        t.ErrorReceived += (sender, e) =>
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Charset = "utf-8";
+            context.Response.ContentEncoding = Encoding.UTF8;
+            context.Response.Write(e.Data);
+        };
+        t.OutputReceived += (sender, e) =>
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Charset = "utf-8";
+            context.Response.ContentEncoding = Encoding.UTF8;
+            context.Response.Write(e.Data);
+        };
+
+        var query = context.Request.QueryString["url"];
+        //context.Response.Write(query);
+        t.Run("echo-html.js", new string[] { query, "--output-encoding=utf-8" });
+    }
+
+    public bool IsReusable
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+}
